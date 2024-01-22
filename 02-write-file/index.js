a2e5ca4f1c7b8be5be6ce650cf360250c42538d0
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-01-22 17:43:05 +0300

    feat: solve 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,17 @@
+const { stdin, stdout } = process;
+const fs = require("fs");
+const path = require("path");
+
+fs.appendFile(path.join(__dirname, "text.txt"), "", (err) => {
+  if (err) console.error(err);
+});
+stdout.write("Enter text: \n");
+stdin.on("data", (text) => {
+  if (text.toString() === "exit\n") process.exit();
+  fs.appendFile(path.join(__dirname, "text.txt"), text, (err) => {
+    if (err) console.error(err);
+  });
+});
+
+process.on("SIGINT", () => process.exit());
+process.on("exit", () => console.log("\nBye!"));
