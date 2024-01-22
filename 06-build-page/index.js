e135736e1fd118aeb4851b2da817626f8d0bac66
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-01-22 18:07:21 +0300

    feat: solve 06-build-page

---
+++ 06-build-page/index.js
@@ -0,0 +1,83 @@
+const { readdir, readFile, mkdir, writeFile } = require("node:fs/promises");
+const path = require("path");
+const { mergeStyles } = require("../05-merge-styles");
+const { copyDirectory } = require("../04-copy-directory");
+
+const getComponentsObj = async () => {
+  try {
+    const components = {};
+    const componentsDir = path.join(__dirname, "components");
+
+    const files = await readdir(componentsDir);
+    for (let file of files) {
+      const [fileName] = file.match(/^\w+/); // drop extention
+      components[fileName] = await readFile(
+        path.join(componentsDir, file),
+        "utf-8"
+      );
+    }
+    return components;
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+const mergeHTML = async () => {
+  try {
+    const componentsObj = await getComponentsObj();
+
+    // read template
+    const template = await readFile(
+      path.join(__dirname, "template.html"),
+      "utf-8"
+    );
+
+    // replace placeholders with components
+    let newTemplate = template;
+    Object.keys(componentsObj).forEach((component) => {
+      newTemplate = newTemplate.replaceAll(
+        `{{${component}}}`,
+        componentsObj[component]
+      );
+    });
+
+    // write to file
+    await writeFile(
+      path.join(__dirname, "project-dist", "index.html"),
+      newTemplate
+    );
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+const copyAssets = async (distDir) => {
+  await mkdir(distDir, { recursive: true });
+
+  const folders = await readdir(path.join(__dirname, "assets"));
+
+  for (let folder of folders) {
+    copyDirectory(
+      path.join(__dirname, "assets", folder),
+      path.join(distDir, "assets", folder)
+    );
+  }
+};
+
+const bundle = async () => {
+  try {
+    const distPath = path.join(__dirname, "project-dist");
+    await mkdir(distPath, { recursive: true });
+
+    mergeHTML();
+    mergeStyles(
+      path.join(__dirname, "styles"),
+      path.join(distPath, "style.css")
+    );
+    copyAssets(distPath);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+bundle();
